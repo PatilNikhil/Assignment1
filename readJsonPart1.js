69ae6c86399e871840d61c7d0d8ae27fd7ff57d8
Nikhil Patil <nikhil.patil@mindstix.com>
2015-03-11 13:41:24 +0530

    Corrections are made

--- readJsonPart1.js
+++ readJsonPart1.js
@@ -1,5 +1,6 @@
 /* Node Module to read json from file and write it to new .txt file  
  @author Nikhil */
+ 
 var fs = require("fs");
 fs.readFile("source.json", function(err, data) {
 	var result = "First Name | Last Name | Score ";
