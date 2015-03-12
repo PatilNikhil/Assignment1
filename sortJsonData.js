8aba6c39489b2924a606e73ca101abb9efa38d0c
Nikhil Patil <nikhil.patil@mindstix.com>
2015-03-12 11:47:48 +0530

    Corrections are done

--- sortJsonData.js
+++ sortJsonData.js
@@ -1,43 +1,29 @@
 /* Node module to read json data and  write sorted data(in descendind) to new .txt File.
  @author Nikhil */
- 
+
 var fs = require("fs");
+var dm = require("./dataManipulation");
+//function of file system to read given file
 fs.readFile("source.json", function(err, data) {
 	var result = " Id | First Name | Last Name | Score ";
 	if(err){
+		//printing error if file not found
 		console.log("File Not Found");		
 	}
 	else{
-		if(IsJsonString(data)){
+		//checking valid json object
+		if(dm.isJsonString(data)){
 			var obj = JSON.parse(data);
-			var sorted = sortArray(obj.students);
+			//calling sortArray for sorting records
+			var sorted = dm.sortArray(obj.students);
+			//concatinating data of each student 
 			for (var i = 0; i < sorted.length; i++) {
 				result += "\n "+ sorted[i].id + " | " + sorted[i].fName + " | " + sorted[i].lName + " | " + sorted[i].score;
 			}
+			//writing final result to .txt file
 			fs.writeFile("destinationFP2.txt", result);
 			console.log("File destinationFP2.txt is created \n Data : \n" + result);
 		}
 	}
 });
 console.log("Reading data from file..!");
-
-function IsJsonString(str) {
-    try {
-        JSON.parse(str);
-    } catch (e) {
-    	console.log("Not a valid Json " + e);
-        return false;
-    }
-    return true;
-}
-
-function sortArray(records) {
-	for (var i = 1; i < records.length; i++){
-		if(records[i].score > records[i-1].score){
-			var temp = records[i];
-			records[i] = records[i-1];
-			records[i-1] = temp;
-		}
-	}
-	return records;
-}
