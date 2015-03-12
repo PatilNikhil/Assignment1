4b550406e65aacb2cc1505f77de5017b8c569fd6
Nikhil Patil <nikhil.patil@mindstix.com>
2015-03-12 11:50:59 +0530

    Corrections are done

--- convertJsonToXml.js
+++ convertJsonToXml.js
@@ -3,47 +3,35 @@
 
 var fs = require("fs");
 var xml = require("xml");
+var dm = require("./dataManipulation");
+//function of file system to read given file
 fs.readFile("source.json", function(err, data) {
 	if(err){
+		//printing error if file not found
 		console.log("File Not Found");		
 	}
 	else{
-		if(IsJsonString(data)){
+		//checking valid json object
+		if(dm.isJsonString(data)){
 			var obj = JSON.parse(data);
-			var sorted = sortArray(obj.students);
+			//calling sortArray for sorting records
+			var sorted = dm.sortArray(obj.students);
+			//creating string of json object
 			var result = '{ "Students": [';
+			//concatinating data of each student 
 			for (var i = 0; i < sorted.length; i++) {
 				result += ' { "Student": [ { "_attr": { "Id": ' + sorted[i].id + '} },{ "Name": "' + sorted[i].fName + ' ' + sorted[i].lName + '"},{ "Score": "' + sorted[i].score + '"} ] }';
+				//concatinating "," upto second last record
 				if(i != sorted.length - 1)	
 				{
 					result += ',';
 				}	
 			}
 			result += '] }';
+			//parsing json sting & passing to Xml() to create xml of object and writing to .xml file
 			fs.writeFile("destinationFP3 .xml", xml(JSON.parse(result), true));
 			console.log("File destinationFP3.xml is created \n Data : \n" + xml(JSON.parse(result), true));
 		}
 	}
 });
 console.log("Reading data from file..!");
-
-function IsJsonString(str) {
-    try {
-        JSON.parse(str);
-    } catch (e) {
-    	console.log("Not a valid Json " + e);
-        return false;
-    }
-    return true;
-}
-
-function sortArray(records) {
-	for (var i = 1; i < records.length; i++){
-		if(records[i].score > records[i-1].score){
-			var temp = records[i];
-			records[i] = records[i-1];
-			records[i-1] = temp;
-		}
-	}
-	return records;
-}
